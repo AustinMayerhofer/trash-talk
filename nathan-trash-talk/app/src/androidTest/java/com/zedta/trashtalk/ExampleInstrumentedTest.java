b4f5ea1f9fd988974a7364382fc6040c9d2aa01f
Nathan J. Chan <njc@nathanjchan.com>
2019-02-09 15:12:06 -0800

    created new project for correct api key

--- nathan-trash-talk/app/src/androidTest/java/com/zedta/trashtalk/ExampleInstrumentedTest.java
+++ nathan-trash-talk/app/src/androidTest/java/com/zedta/trashtalk/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.trashtalk", appContext.getPackageName());
+        assertEquals("com.zedta.trashtalk", appContext.getPackageName());
     }
 }
